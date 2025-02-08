863e107d5a656d7effe4ca1e4d9247510068e599
Mohamed El Kartit <medkr2002@gmail.com>
2025-02-08 02:06:10 +0100

    storage file access

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
+Route::get('/storage/{path}', function ($path) {
+    if (Storage::disk('public')->exists($path)) {
+        return response()->file(storage_path("app/public/{$path}"));
+    }
+    abort(404);
+})->where('path', '.*');
 Route::get('/', function () {
     return view('welcome');
 });
