c2bce72f80f7478ab2bfced014b9e63d3405878e
Mohamed El Kartit <medkr2002@gmail.com>
2025-02-08 20:55:46 +0100

    change the route

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::apiResource("announces", ApiController::class);
+Route::middleware('auth:sanctum')->group(function () {
+Route::apiResource("announces", ApiController::class); });
