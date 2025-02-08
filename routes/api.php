4b1e8747b08c4ef202ad3b577fb889cc652ed32b
Mohamed El Kartit <medkr2002@gmail.com>
2025-02-08 18:32:44 +0100

    modify the upload images

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::apiResource("announces", ApiController::class);
